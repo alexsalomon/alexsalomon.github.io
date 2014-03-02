179e03e79c52912e70f93aa2f6abb1b89bbd4c22
Alex Salomon <alexsalomon.th@gmail.com>
2014-03-02 15:36:50 -0600

    Add rounded gravatar image to home page

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,4 +1,22 @@
 
+$(document).ready(function() 
+{
+	//load gravatar image
+	var gravatar_image_url = get_gravatar_image_url('alexsalomon.th@gmail.com', 150); //need to change img-circle-border css to change image size
+	$('#gravatar_image').attr('src', gravatar_image_url);
+});
+
+function get_gravatar_image_url(email, size, default_image, allowed_rating, force_default)
+{
+	email = typeof email !== 'undefined' ? email : 'alexsalomon.th@gmail.com';
+	size = (size >= 1 && size <= 2048) ? size : 80;
+	default_image = typeof default_image !== 'undefined' ? default_image : 'mm';
+	allowed_rating = typeof allowed_rating !== 'undefined' ? allowed_rating : 'x';
+	force_default = force_default === true ? 'y' : 'n';
+
+	return ("http://www.gravatar.com/avatar/" + md5(email.toLowerCase().trim()) + "?size=" + size + "&default=" + encodeURIComponent(default_image) + "&rating=" + allowed_rating + (force_default === 'y' ? "&forcedefault=" + force_default : ''));
+}
+   
 function change_image_source_on_hover(element, nohover_image, hover_image)
 {
 	$("#" + element).hover(
