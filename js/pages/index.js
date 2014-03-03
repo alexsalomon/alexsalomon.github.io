3a09ed0df5bb3eeccd3f84f22b88fffece6201d3
Alex Salomon <alexsalomon.th@gmail.com>
2014-03-02 19:20:37 -0600

    Cleaned design by changing font and reducing size of images

--- js/pages/index.js
+++ js/pages/index.js
@@ -2,7 +2,7 @@
 $(document).ready(function() 
 {
 	//load gravatar image
-	var gravatar_image_url = get_gravatar_image_url('alexsalomon.th@gmail.com', 150); //need to change img-circle-border css to change image size
+	var gravatar_image_url = get_gravatar_image_url('alexsalomon.th@gmail.com', 135); //need to change img-circle-border css to change image size
 	$('#gravatar_image').attr('src', gravatar_image_url);
 });
 
@@ -32,4 +32,4 @@ function change_image_source_on_hover(element, nohover_image, hover_image)
 change_image_source_on_hover("linkedin", "assets/icons/LinkedIn_grey.jpg", "assets/icons/LinkedIn_color.jpg");
 change_image_source_on_hover("github", "assets/icons/Github_grey.jpg", "assets/icons/Github_color.jpg");
 change_image_source_on_hover("instagram", "assets/icons/Instagram_grey.jpg", "assets/icons/Instagram_color.jpg");
-change_image_source_on_hover("email", "assets/icons/email_color.png", "assets/icons/email_color.png");
+change_image_source_on_hover("email", "assets/icons/email_grey.jpg", "assets/icons/email_color.jpg");
